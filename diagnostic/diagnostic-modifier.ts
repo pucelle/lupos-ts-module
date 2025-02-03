c1f837d0eb097c93c9586def4587e1182fddd850
Freeman Chen <pucelle@163.com>
2025-02-03 18:30:04 +0800

    No need to set start diagnostics now.

--- diagnostic/diagnostic-modifier.ts
+++ diagnostic/diagnostic-modifier.ts
@@ -1,6 +1,5 @@
 import type * as TS from 'typescript'
 import {Helper} from '../helper'
-import {PairKeysMap} from '../utils'
 import {DiagnosticCode} from './codes'
 
 
@@ -8,27 +7,17 @@ import {DiagnosticCode} from './codes'
 export class DiagnosticModifier {
 	readonly helper: Helper
 
-	protected startDiagnostics!: TS.Diagnostic[]
 	protected sourceFile!: TS.SourceFile
-
-	protected diagnosticsByStartAndCode: PairKeysMap<number, number, TS.Diagnostic> = new PairKeysMap()
 	protected added: TS.Diagnostic[] = []
-	protected deleted: TS.Diagnostic[] = []
+	protected deleted: {start: number, code: number}[] = []
 
 	constructor(helper: Helper) {
 		this.helper = helper
 	}
 
 	/** Initialize before visit a new source file. */
-	setStart(startDiagnostics: TS.Diagnostic[], sourceFile: TS.SourceFile) {
-		this.startDiagnostics = startDiagnostics
+	setSourceFile(sourceFile: TS.SourceFile) {
 		this.sourceFile = sourceFile
-
-		for (let diag of this.startDiagnostics) {
-			if (diag.start !== undefined) {
-				this.diagnosticsByStartAndCode.set(diag.start, diag.code, diag)
-			}
-		}
 	}
 
 	/** Add a diagnostic object. */
@@ -37,20 +26,11 @@ export class DiagnosticModifier {
 			return
 		}
 
-		if (this.diagnosticsByStartAndCode.has(diag.start, diag.code)) {
-			return
-		}
-
 		this.added.push(diag)
-		this.diagnosticsByStartAndCode.set(diag.start, diag.code, diag)
 	}
 
 	/** Add a diagnostic by parameters. */
 	add(start: number, length: number, code: DiagnosticCode, message: string, category: TS.DiagnosticCategory = this.helper.ts.DiagnosticCategory.Error) {
-		if (this.diagnosticsByStartAndCode.has(start, code)) {
-			return
-		}
-
 		let diag: TS.Diagnostic = {
 			category,
 			code,
@@ -61,7 +41,6 @@ export class DiagnosticModifier {
 		}
 
 		this.added.push(diag)
-		this.diagnosticsByStartAndCode.set(start, code, diag)
 	}
 
 	/** 
@@ -76,14 +55,10 @@ export class DiagnosticModifier {
 			let importDecl = node.parent.parent.parent
 
 			if (this.helper.ts.isImportDeclaration(importDecl)) {
-				let start = importDecl.getStart()
-				let diag = this.diagnosticsByStartAndCode.get(start, 6133)
-				if (diag) {
-					this.deleteOfNode(importDecl, [6133])
+				this.deleteOfNode(importDecl, [6133])
 
-					// Note not return here, all imports, and specified
-					// import diagnostics exist at the same time.
-				}
+				// Note not return here, all imports, and specified
+				// import diagnostics exist at the same time.
 			}
 		}
 
@@ -98,17 +73,16 @@ export class DiagnosticModifier {
 		let start = node.getStart()
 
 		for (let code of codes) {
-			let diag = this.diagnosticsByStartAndCode.get(start, code)
-			if (diag) {
-				this.diagnosticsByStartAndCode.delete(start, code)
-				this.deleted.push(diag)
-			}
+			this.deleted.push({start, code})
 		}
 	}
 
 	/** Get all diagnostics after modified. */
-	getModified() {
-		let diags = this.startDiagnostics.filter(diag => !this.deleted.includes(diag))
+	getModified(startDiagnostics: TS.Diagnostic[]) {
+		let diags = startDiagnostics.filter(diag => () => {
+			return !this.deleted.find(item => item.start === diag.start && item.code === diag.code)
+		})
+
 		return [...diags, ...this.added]
 	}
 }
