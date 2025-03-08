8c2ddaba84a23a8a52e6befdf5d71d922b564290
Freeman Chen <pucelle@163.com>
2025-03-08 09:40:10 +0800

    Try solve generic type checking

--- diagnostic/of-parts/event.ts
+++ diagnostic/of-parts/event.ts
@@ -41,6 +41,8 @@ export function diagnoseEvent(
 					let fromText = helper.types.getTypeFullText(handlerType)
 					let toText = helper.types.getTypeFullText(eventType)
 	
+					helper.types.isAssignableToExtended(handlerType, eventType)
+
 					modifier.add(start, length, DiagnosticCode.NotAssignable, `Property type "${fromText}" is not assignable to type "${toText}".`)
 					return
 				}
