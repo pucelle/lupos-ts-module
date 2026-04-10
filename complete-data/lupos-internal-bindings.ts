1b5fe686a4cbe7c9695e12805f6d8757c4be1d63
Freeman Chen <pucelle@163.com>
2026-04-10 17:07:26 +0800

    Removes `:slot`

--- complete-data/lupos-internal-bindings.ts
+++ complete-data/lupos-internal-bindings.ts
@@ -3,7 +3,6 @@ export const LuposKnownInternalBindings: Record<string, {name: string, parameter
 	class: {name: 'ClassBinding', parameterCount: 1, implementsPart: false},
 	html: {name: 'HTMLBinding', parameterCount: 1, implementsPart: false},
 	ref: {name: 'RefBinding', parameterCount: 3, implementsPart: true},
-	slot: {name: 'SlotBinding', parameterCount: 1, implementsPart: true},
 	style: {name: 'StyleBinding', parameterCount: 1, implementsPart: false},
 	transition: {name: 'TransitionBinding', parameterCount: 3, implementsPart: true},
 }
